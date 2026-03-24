034b6bb90ab839e405e75ebc9fc453705a859ad2
Ger726 <vesromka@gmail.com>
2026-03-25 01:17:22 +0300

    Create task1.js

---
+++ 2026-02-05/task1.js
@@ -0,0 +1,13 @@
+/ 1. Проверка числа
+// Дано n = -5. Вывести:
+// "positive" если > 0, "negative" если < 0, "zero" если == 0
+
+const n = -5;
+
+if (n > 0) {
+  console.log("positive");
+} else if (n < 0) {
+  console.log("negative");
+} else {
+  console.log("zero");
+}
